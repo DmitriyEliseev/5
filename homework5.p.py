248d597bbcd4a45940b1d772ed032c7284ad0a26
DmitriyEliseev <93958489+DmitriyEliseev@users.noreply.github.com>
2024-07-24 13:50:51 +0300

    Add files via upload

---
+++ homework5.p.py
@@ -0,0 +1,9 @@
+immutable_var = 1, 2, 3, 'ok'
+print(immutable_var)
+#immutable_var[1] = 200
+#print(immutable_var)
+#кортеж относится к неизменяемым типам данных
+mutable_list = [1, 2, 'ok']
+print (mutable_list)
+mutable_list[2] = 'not ok'
+print (mutable_list)
